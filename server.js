e9f633dcb64b5045e302e4d17539d4762a748777
Aramide-jk <aramjay29@gmail.com>
2025-09-22 22:08:32 +0100

    updated

--- server.js
+++ server.js
@@ -17,8 +17,19 @@ const inspectionRoutes_1 = __importDefault(
 );
 const paymentRoutes_1 = __importDefault(require("./routes/paymentRoutes"));
 const errorMiddleware_1 = require("./middleware/errorMiddleware");
+// Load env vars for development
 dotenv_1.default.config();
-(0, db_1.default)();
+// Connect to Database
+(0, db_1.default)().then(() => {
+  // Start server only if DB connection is successful and not in test environment
+  if (process.env.NODE_ENV !== "test") {
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      );
+    });
+  }
+});
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 
@@ -42,10 +53,4 @@ app.use("/api/payment", paymentRoutes_1.default);
 app.use(errorMiddleware_1.notFound);
 app.use(errorMiddleware_1.errorHandler);
 const PORT = process.env.PORT || 5000;
-// Start server only if not in test environment
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server running in development mode on port ${PORT}`);
-  });
-}
 exports.default = app;
